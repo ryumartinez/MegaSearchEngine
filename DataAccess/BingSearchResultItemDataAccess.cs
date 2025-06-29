e2a4a89a1515fe4f5f16519d8639ab842539ee68
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-06-29 18:49:58 -0300

    updated bing and google data accessors

--- DataAccess/BingSearchResultItemDataAccess.cs
+++ DataAccess/BingSearchResultItemDataAccess.cs
@@ -1,21 +1,51 @@
+using System.Net.Http;
+using System.Web;
 using DataAccess.Contract.SearchResultItem;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess;
 
 public class BingSearchResultItemDataAccess : ISearchResultItemDataAccess
 {
-    public Task<IEnumerable<SearchResultArticleAccessModel>> GetArticlesAsync(GetSearchResultItemAccessRequest request)
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+
+    public BingSearchResultItemDataAccess(HttpClient httpClient, IConfiguration config)
     {
-        throw new NotImplementedException();
+        _httpClient = httpClient;
+        _apiKey = config["Bing:ApiKey"] ?? throw new InvalidOperationException("Bing API key not configured.");
+        _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+    }
+
+    public async Task<IEnumerable<SearchResultArticleAccessModel>> GetArticlesAsync(GetSearchResultItemAccessRequest request)
+    {
+        var url = BuildSearchUrl(request.SearchText);
+        var response = await _httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+        // TODO: Deserialize and map to SearchResultArticleAccessModel
+        return Enumerable.Empty<SearchResultArticleAccessModel>();
     }
 
     public Task<IEnumerable<SearchResultImageAccessModel>> GetImagesAsync(GetSearchResultItemAccessRequest request)
     {
-        throw new NotImplementedException();
+        // Use Bing Image Search API
+        return Task.FromResult(Enumerable.Empty<SearchResultImageAccessModel>());
     }
 
     public Task<IEnumerable<SearchResultVideoAccessModel>> GetVideosAsync(GetSearchResultItemAccessRequest request)
     {
-        throw new NotImplementedException();
+        // Use Bing Video Search API
+        return Task.FromResult(Enumerable.Empty<SearchResultVideoAccessModel>());
+    }
+
+    private string BuildSearchUrl(string query)
+    {
+        var builder = new UriBuilder("https://api.bing.microsoft.com/v7.0/search");
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["q"] = query;
+        builder.Query = queryString.ToString();
+        return builder.ToString();
     }
 }
--- DataAccess/GoogleSearchResultItemDataAccess.cs
+++ DataAccess/GoogleSearchResultItemDataAccess.cs
@@ -1,21 +1,56 @@
+using System.Net.Http;
+using System.Web;
 using DataAccess.Contract.SearchResultItem;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess;
 
-internal class GoogleSearchResultItemDataAccess : ISearchResultItemDataAccess
+public class GoogleSearchResultItemDataAccess : ISearchResultItemDataAccess
 {
-    public Task<IEnumerable<SearchResultArticleAccessModel>> GetArticlesAsync(GetSearchResultItemAccessRequest request)
+    private readonly HttpClient _httpClient;
+    private readonly string _apiKey;
+    private readonly string _cseId;
+
+    public GoogleSearchResultItemDataAccess(HttpClient httpClient, IConfiguration config)
     {
-        throw new NotImplementedException();
+        _httpClient = httpClient;
+        _apiKey = config["Google:ApiKey"] ?? throw new InvalidOperationException("Google API key not configured.");
+        _cseId = config["Google:CseId"] ?? throw new InvalidOperationException("Google CSE ID not configured.");
+    }
+
+    public async Task<IEnumerable<SearchResultArticleAccessModel>> GetArticlesAsync(GetSearchResultItemAccessRequest request)
+    {
+        var url = BuildSearchUrl(request.SearchText, request.PageIndex, request.PageSize);
+        var response = await _httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+        // TODO: Deserialize and map to SearchResultArticleAccessModel
+        return Enumerable.Empty<SearchResultArticleAccessModel>();
     }
 
     public Task<IEnumerable<SearchResultImageAccessModel>> GetImagesAsync(GetSearchResultItemAccessRequest request)
     {
-        throw new NotImplementedException();
+        // Use Google Images Search (same endpoint, add `searchType=image`)
+        return Task.FromResult(Enumerable.Empty<SearchResultImageAccessModel>());
     }
 
     public Task<IEnumerable<SearchResultVideoAccessModel>> GetVideosAsync(GetSearchResultItemAccessRequest request)
     {
-        throw new NotImplementedException();
+        // Not directly supported by CSE, usually handled via YouTube API
+        return Task.FromResult(Enumerable.Empty<SearchResultVideoAccessModel>());
+    }
+
+    private string BuildSearchUrl(string query, string pageIndex, string pageSize)
+    {
+        var builder = new UriBuilder("https://www.googleapis.com/customsearch/v1");
+        var queryString = HttpUtility.ParseQueryString(string.Empty);
+        queryString["key"] = _apiKey;
+        queryString["cx"] = _cseId;
+        queryString["q"] = query;
+        queryString["start"] = pageIndex;
+        queryString["num"] = pageSize;
+        builder.Query = queryString.ToString();
+        return builder.ToString();
     }
 }
