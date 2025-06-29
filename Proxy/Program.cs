9c0a7321beb3e572f039544e9f1466546354392d
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-06-29 14:33:42 -0300

    configured proxy

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver();
 
 var app = builder.Build();
 
