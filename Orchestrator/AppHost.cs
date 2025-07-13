6b02d5195c3fcd633dd7e058b0aa6ac3bf19bf34
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-07-13 11:09:47 -0300

    key vault working

--- Orchestrator/AppHost.cs
+++ Orchestrator/AppHost.cs
@@ -1,13 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var existingKeyVaultName = builder.AddParameter("existingKeyVaultName");
-var existingKeyVaultResourceGroup = builder.AddParameter("existingKeyVaultResourceGroup");
+var keyVault = builder.AddAzureKeyVault("my-api-secrets");
 
-var keyVault = builder.AddAzureKeyVault("keyVault")
-    .AsExisting(existingKeyVaultName, existingKeyVaultResourceGroup);
-
-var api = builder.AddProject<Projects.Api>("api")
-    .WithReference(keyVault);
+var api = builder.AddProject<Projects.Api>("api").WithReference(keyVault);
 
 builder.AddProject<Projects.Proxy>("proxy").WithReference(api);
 
