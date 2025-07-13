0c45f322364e8222f332b10910e0316e99011528
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-07-13 09:52:30 -0300

    updated appsettings

--- Orchestrator/AppHost.cs
+++ Orchestrator/AppHost.cs
@@ -1,6 +1,12 @@
+using Azure.Identity;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var keyVault = builder.AddAzureKeyVault("key-vault");
+var existingKeyVaultName = builder.AddParameter("existingKeyVaultName");
+var existingKeyVaultResourceGroup = builder.AddParameter("existingKeyVaultResourceGroup");
+
+var keyVault = builder.AddAzureKeyVault("keyVault")
+    .AsExisting(existingKeyVaultName, existingKeyVaultResourceGroup);
 
 builder.AddProject<Projects.Api>("api")
     .WithReference(keyVault);
