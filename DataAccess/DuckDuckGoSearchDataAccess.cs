532b907c0c33f0cb29e9b0f23525deb7fc29b4fe
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-08-01 17:43:28 -0300

    updated DuckDuckGoSearchDataAccess

--- DataAccess/DuckDuckGoSearchDataAccess.cs
+++ DataAccess/DuckDuckGoSearchDataAccess.cs
@@ -1,36 +1,47 @@
 using System.Net.Http;
 using System.Text.Json;
 using DataAccess.Contract.SearchResultItem;
+using HtmlAgilityPack;
 using Microsoft.Extensions.Configuration;
 
 namespace DataAccess;
 
 public class DuckDuckGoSearchDataAccess : ISearchDataAccess
 {
-    private readonly HttpClient _httpClient;
-    private readonly string _apiKey;
-
-    public DuckDuckGoSearchDataAccess(HttpClient httpClient, IConfiguration config)
-    {
-        _httpClient = httpClient;
-        _apiKey = config["SerpApi:ApiKey"] ?? throw new InvalidOperationException("SerpAPI key not configured.");
-    }
-
     public async Task<IEnumerable<SearchResultAccessModel>> SearchAsync(SearchAccessRequest request)
     {
-        var json = await FetchSearchResultsAsync(request, "duckduckgo");
-        // TODO: Map results to SearchResultAccessModel
-        return Enumerable.Empty<SearchResultAccessModel>();
-    }
+        var baseUrl = "https://html.duckduckgo.com/html/";
+        var query = Uri.EscapeDataString(request.SearchText);
+        var page = int.TryParse(request.PageIndex, out var pageIndex) ? pageIndex : 0;
 
-    private async Task<string> FetchSearchResultsAsync(SearchAccessRequest request, string engine)
-    {
-        var url = $"https://serpapi.com/search.json?q={Uri.EscapeDataString(request.SearchText)}" +
-                  $"&engine={engine}&api_key={_apiKey}&start={request.PageIndex}&num={request.PageSize}";
+        var httpClient = new HttpClient();
+        var formData = new FormUrlEncodedContent(new[]
+        {
+            new KeyValuePair<string, string>("q", request.SearchText),
+        });
 
-        var response = await _httpClient.GetAsync(url);
-        response.EnsureSuccessStatusCode();
+        // DuckDuckGo's HTML version requires POST for search
+        var response = await httpClient.PostAsync(baseUrl, formData);
+        var html = await response.Content.ReadAsStringAsync();
 
-        return await response.Content.ReadAsStringAsync();
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(html);
+
+        var resultNodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='result']");
+
+        var results = resultNodes.Select(node =>
+        {
+            var titleNode = node.SelectSingleNode(".//a[contains(@class,'result__a')]");
+            var snippetNode = node.SelectSingleNode(".//a[contains(@class,'result__snippet')]");
+
+            var title = titleNode?.InnerText?.Trim() ?? "No Title";
+            var link = titleNode?.GetAttributeValue("href", "#") ?? "#";
+            var description = snippetNode?.InnerText?.Trim() ?? "No Description";
+
+            return new SearchResultAccessModel(title, description, link);
+        });
+
+        // Apply paging manually (not supported by URL param)
+        return results.Skip(page * int.Parse(request.PageSize)).Take(int.Parse(request.PageSize));
     }
 }
