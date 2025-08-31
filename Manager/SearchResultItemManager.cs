977a923b3fbcb5cae42cc0a6da77c78cae836b02
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-08-31 12:37:25 -0300

    added FarmacenterSearchDataAccess
    currently playwright is blocked by the site

--- Manager/SearchResultItemManager.cs
+++ Manager/SearchResultItemManager.cs
@@ -6,13 +6,14 @@ namespace Manager;
 
 public class SearchResultItemManager(
     [FromKeyedServices("duckduckgo")] ISearchDataAccess duckDuckGoSearchDataAccess,
-    [FromKeyedServices("puntofarma")] ISearchDataAccess puntoFarmaSearchDataAccess
+    [FromKeyedServices("puntofarma")] ISearchDataAccess puntoFarmaSearchDataAccess,
+    [FromKeyedServices("farmacenter")] ISearchDataAccess farmacenterSearchDataAccess
     ) : ISearchResultItemManager
 {
     public async Task<IEnumerable<SearchResultItemModel>> GetAsync(string searchText)
     {
         var request = new SearchAccessRequest(1,1, searchText);
-        var result = await puntoFarmaSearchDataAccess.SearchAsync(request);
+        var result = await farmacenterSearchDataAccess.SearchAsync(request);
         var mappedResult = result
             .Select(x => 
                 new SearchResultItemModel(
