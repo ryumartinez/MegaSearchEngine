6ad78815f668d55ec25d2454242bee59f8464bad
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-10-12 18:19:54 -0300

    added initial layout

--- Frontend/admin-panel/src/components/link.tsx
+++ Frontend/admin-panel/src/components/link.tsx
@@ -1,14 +1,36 @@
 import * as Headless from '@headlessui/react'
-import NextLink, { type LinkProps } from 'next/link'
 import React, { forwardRef } from 'react'
+import {
+  Link as RouterLink,
+  type LinkProps as RouterLinkProps,
+} from '@tanstack/react-router'
 
-export const Link = forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>
+/**
+ * AppLink
+ * - Preferred: pass `to="/route"` (TanStack Router)
+ * - Backcompat: if only `href` is provided, it will be mapped to `to`
+ * - Forwards a ref to the underlying <a>
+ */
+type AnchorProps = React.ComponentPropsWithoutRef<'a'>
+
+type AppLinkProps =
+  | (RouterLinkProps & AnchorProps) // normal TS Router usage
+  | (AnchorProps & { href: string; to?: string }) // backcompat: href-only
+
+export const Link = forwardRef<HTMLAnchorElement, AppLinkProps>(function Link(
+  props,
+  ref
 ) {
+  // Normalize `href` -> `to` when `to` isn't provided
+  const normalized =
+    'href' in props && !('to' in props) && typeof props.href === 'string'
+      ? ({ ...props, to: props.href } as RouterLinkProps & AnchorProps)
+      : (props as RouterLinkProps & AnchorProps)
+
   return (
     <Headless.DataInteractive>
-      <NextLink {...props} ref={ref} />
+      <RouterLink {...normalized} ref={ref} />
     </Headless.DataInteractive>
   )
 })
+
--- Frontend/admin-panel/src/routes/__root.tsx
+++ Frontend/admin-panel/src/routes/__root.tsx
@@ -3,23 +3,29 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
 
 import Header from '../components/Header'
+import {ApplicationLayout} from "@/layouts/application-layout.tsx";
+import {getEvents} from "@/data.ts";
+
+let events = await getEvents()
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Header />
-      <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
+      <ApplicationLayout events={events}>
+        <Header />
+        <Outlet />
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+      </ApplicationLayout>
     </>
   ),
 })
