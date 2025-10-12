0d4cbcd58fbb86229da44b751c68206b6a986658
Ryu Martinez <ryueduardomartinez@outlook.es>
2025-10-12 17:52:33 -0300

    updated vite.config.ts

--- Frontend/admin-panel/vite.config.ts
+++ Frontend/admin-panel/vite.config.ts
@@ -1,16 +1,16 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nitroV2Plugin(),
-    TanStackRouterVite({ autoCodeSplitting: true }),
+    tanstackRouter({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
