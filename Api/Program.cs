667ee14d9e52970ea0b3c98de861d3c4134a043e
Ryu Martinez <ryueduardomartinez@outlook.es>
2026-01-25 12:36:30 -0300

    added TickerQ

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,11 +3,28 @@ using DataAccess.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using Utils;
+using TickerQ.Dashboard.DependencyInjection;
+using TickerQ.DependencyInjection;
+using TickerQ.EntityFrameworkCore.Customizer;
+using TickerQ.EntityFrameworkCore.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddOpenApi();
+
+builder.Services.AddTickerQ(options =>
+{
+    options.AddOperationalStore(ef => 
+    {
+        ef.UseApplicationDbContext<AppDbContext>(ConfigurationType.UseModelCustomizer);
+    });
+    options.AddDashboard(dashboardOptions =>
+    {
+        dashboardOptions.SetBasePath("/admin/tickerq");
+    });
+});
+
 builder.Services.AddOptions<PlaywrightOptions>()
     .Bind(builder.Configuration.GetSection(PlaywrightOptions.SectionName))
     .ValidateDataAnnotations()
@@ -18,17 +35,17 @@ Engine.ServiceInjection.ConfigureServices(builder.Services);
 Manager.ServiceInjection.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+app.UseTickerQ();
 
 using var scope = app.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-// Apply any pending migrations
 await dbContext.Database.MigrateAsync().ConfigureAwait(false);
 
-// Configure the HTTP request pipeline.
 app.MapOpenApi();
 app.MapScalarApiReference();
 app.UseHttpsRedirection();
+
 app.MapSearchEndpoints();
 
 await app.RunAsync().ConfigureAwait(false);
