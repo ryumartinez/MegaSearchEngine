cfcc9803c9e35bc826e1e2fc314076d53d8c6a20
Ryu Martinez <ryueduardomartinez@outlook.es>
2026-01-25 10:39:06 -0300

    FEAT: open observe working

--- Orchestrator/AppHost.cs
+++ Orchestrator/AppHost.cs
@@ -20,11 +20,23 @@ var playwrightBrowser = builder
     // Explicitly tell npx to use version 1.54.0 to match your client NuGet package.
     .WithArgs("-c", "npx playwright@1.54.0 run-server --port=9222 --host=0.0.0.0");
 
+var openObserve = builder.AddContainer("openobserve", "public.ecr.aws/zinclabs/openobserve", "latest")
+    .WithHttpEndpoint(targetPort: 5080, name: "ui")
+    .WithEnvironment("ZO_ROOT_USER_EMAIL", "admin@example.com")
+    .WithEnvironment("ZO_ROOT_USER_PASSWORD", "ComplexPass123")
+    .WithBindMount("./openobserve-data", "/data"); // Persist your data
+
+var openObserveEndpoint = openObserve.GetEndpoint("ui");
+
 var api = builder
     .AddProject<Projects.Api>("api")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", $"{openObserve.GetEndpoint("ui")}/api/default") 
+    .WithEnvironment("OTEL_EXPORTER_OTLP_HEADERS", "Authorization=Basic YWRtaW5AZXhhbXBsZS5jb206Q29tcGxleFBhc3MxMjM=")
+    .WithEnvironment("OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf")
     .WithReference(keyVault)
     .WithEnvironment("Playwright__CdpEndpoint", playwrightBrowser.GetEndpoint("cdp"))
     .WithReference(postgresdb)
+    .WaitFor(openObserve)
     .WaitFor(playwrightBrowser)
     .WaitFor(postgresdb)
     .WaitFor(keyVault);
